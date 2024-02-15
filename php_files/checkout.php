edf8cfe02579fa50274ab865efdba17c6eb91460
Arvid <31167802+Arvid6@users.noreply.github.com>
2024-02-15 10:39:06 +0100

    style

---
+++ php_files/checkout.php
@@ -0,0 +1,52 @@
+<?php
+
+$servername = "utbweb.its.ltu.se";
+$username = "19990921";
+$password = "Projekt99!";
+$dbname = "db19990921";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+
+
+?>
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Checkout</title>
+</head>
+
+<body>
+<header>
+    <nav>
+        <ul>
+            <li><a class="link" href="index.php">START</a></li>
+            <li><a class="link" href="checkout.php">CHCKOUT</a></li>
+            <li><a class="link" href="registration.php">REGISTRATE</a></li>
+        </ul>
+    </nav>
+</header>
+
+<main id="order">
+<p> HEJ HEJ HEJ HÄR KAN MAN SÄTTA IN INFO</p>
+
+</main>
+<br>
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script text="text/javscript" src="script.js"></script>
+</body>
+</html>
+
