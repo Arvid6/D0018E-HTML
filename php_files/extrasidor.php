3648b34cd160681fc4884d71dca047af381c5927
Lynxfor <102663128+Gabriel-Sundblad@users.noreply.github.com>
2024-02-29 19:41:26 +0100

    working add to cart

--- php_files/extrasidor.php
+++ php_files/extrasidor.php
@@ -4,14 +4,70 @@
 
     session_start();
     include("connect.php"); 
-    $cart_id = $_SESSION['cart_id'];
 
-    $product_id = $_GET['id']; 
-    ?>
+   
+    
+if(isset($_SESSION['userId'])){
+    $user_id = implode($_SESSION['userId']);
+
+    $checkUser = "SELECT cart_id FROM cart WHERE UserId = $user_id";
+    $refresh_block = $conn->query($checkUser);
+    if((!$refresh_block->num_rows > 0)) {
+        $conn->query("INSERT INTO cart (UserId) VALUES ($user_id)");
+        echo("Ingen vagn");
+    }
+    // Fetch the user cart id
+    $sql = $conn->query("SELECT cart_id FROM cart WHERE UserId = $user_id");
+    $fetch = $sql->fetch_assoc();
+    $cart_id = $fetch['cart_id'];
+
+}else{
+    $session_id = session_id();
+
+    $check_session = "SELECT cart_id FROM cart WHERE session_id = '$session_id'";
+    $q = $conn->query($check_session);
+    if((!$q->num_rows>0)) {
+        $conn->query("INSERT INTO cart (session_id) VALUES ('$session_id')");
+    }
+    // Fetch the session cart id
+    $sql = $conn->query("SELECT cart_id FROM cart WHERE session_id = '$session_id'");
+    $fetch = $sql->fetch_assoc();
+    $cart_id = $fetch['cart_id'];
+}
+$product_id = $_GET['id']; 
 
 
+if( isset($_GET['add']) )
+            {
+                //be sure to validate and clean your variables
+                $prod = htmlentities($_GET['id']);
 
-<?php
+                // Check if the product already exists in the cart
+                $already_in_cart_query = "SELECT product_id FROM cart_items WHERE cart_id = $cart_id and product_id = $prod";
+                $already_in_cart = $conn->query($already_in_cart_query);
+
+                // Get product price when clicked
+                $price_query = $conn->query("SELECT price FROM product WHERE product_id= $prod");
+                $fetch_price = $price_query->fetch_assoc();
+                $prod_price =$fetch_price['price'];
+
+                $update_stock = "UPDATE product SET stock = stock - 1 WHERE product_id = $prod";
+
+                if($already_in_cart->num_rows > 0) {
+                    $update_quantity = "UPDATE cart_items SET quantity = quantity + 1 WHERE cart_id = $cart_id AND product_id = $prod";
+                    $conn->query($update_stock);
+                    $conn->query($update_quantity);
+
+                }else {
+                    $add_cart_item = "INSERT INTO cart_items (cart_id, product_id, quantity, price) VALUES($cart_id, $prod, 1, $prod_price)";
+                    echo($add_cart_item);
+                    $conn->query($update_stock);
+                    $conn->query($add_cart_item);
+                }
+                header("Location:extrasidor.php?id=$product_id");
+
+            }
+
     include("onatop.php"); 
 
     $sql = "SELECT * FROM product WHERE product_id = $product_id";
