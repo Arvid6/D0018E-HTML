06c30201a351a7db92f9c8eb277264bb8f82828e
EricVikberg <erivik-8@student.ltu.se>
2024-03-06 13:19:51 +0100

    Product pages doesnt book assets anymore

--- php_files/extrasidor.php
+++ php_files/extrasidor.php
@@ -37,35 +37,69 @@ if(isset($_SESSION['userId'])){
 
 $product_id = $_GET['id'];
 
+$items_in_cart = array(0);
+
+$sql = "SELECT * FROM product";
+$res = $conn->query($sql);
+$i = 1;
+if($res->num_rows > 0) {
+
+    while($row = $res->fetch_assoc()) {
+        $check_cart = $conn->query("SELECT product_id FROM cart_items WHERE cart_id = $cart_id and product_id = $i");
+
+        if($check_cart->num_rows > 0 ) {
+            $get_quant = $conn->query("SELECT quantity FROM cart_items WHERE cart_id = $cart_id AND product_id = $i");
+            $gq_fetch = $get_quant->fetch_assoc();
+            $quant = $gq_fetch['quantity'];
+            array_push($items_in_cart, $quant);
+        }else{
+            array_push($items_in_cart, 0);
+        }
+
+        $i = $i + 1;
+    }
+}
+$_SESSION['items_in_cart'] = $items_in_cart;
 
 if( isset($_GET['add']) )
-            {
-                
-                $prod = htmlentities($_GET['id']);
+{
+    //be sure to validate and clean your variables
+    $prod = htmlentities($_GET['id']);
 
-                $already_in_cart_query = "SELECT product_id FROM cart_items WHERE cart_id = $cart_id and product_id = $prod";
-                $already_in_cart = $conn->query($already_in_cart_query);
+    // Check if the product already exists in the cart
+    $already_in_cart_query = "SELECT product_id FROM cart_items WHERE cart_id = $cart_id and product_id = $prod";
+    $already_in_cart = $conn->query($already_in_cart_query);
 
-                $price_query = $conn->query("SELECT price FROM product WHERE product_id= $prod");
-                $fetch_price = $price_query->fetch_assoc();
-                $prod_price =$fetch_price['price'];
+    // Get product price when clicked
+    $price_query = $conn->query("SELECT price FROM product WHERE product_id= $prod");
+    $fetch_info = $price_query->fetch_assoc();
+    $prod_price =$fetch_info['price'];
 
-                $update_stock = "UPDATE product SET stock = stock - 1 WHERE product_id = $prod";
+    //$update_stock = "UPDATE product SET stock = stock - 1 WHERE product_id = $prod";
 
-                if($already_in_cart->num_rows > 0) {
-                    $update_quantity = "UPDATE cart_items SET quantity = quantity + 1 WHERE cart_id = $cart_id AND product_id = $prod";
-                    $conn->query($update_stock);
-                    $conn->query($update_quantity);
+    if($already_in_cart->num_rows > 0) {
+        $update_quantity = "UPDATE cart_items SET quantity = quantity + 1 WHERE cart_id = $cart_id AND product_id = $prod";
+        //$conn->query($update_stock);
+        $conn->query($update_quantity);
 
-                }else {
-                    $add_cart_item = "INSERT INTO cart_items (cart_id, product_id, quantity, price) VALUES($cart_id, $prod, 1, $prod_price)";
-                    echo($add_cart_item);
-                    $conn->query($update_stock);
-                    $conn->query($add_cart_item);
-                }
-                header("Location:extrasidor.php?id=$product_id"); // det ända som behövde ändras
+    }else {
+        $add_cart_item = "INSERT INTO cart_items (cart_id, product_id, quantity, price) VALUES($cart_id, $prod, 1, $prod_price)";
+        echo($add_cart_item);
+        //$conn->query($update_stock);
+        $conn->query($add_cart_item);
+    }
 
-            }
+    $get_quant = $conn->query("SELECT quantity FROM cart_items WHERE cart_id = $cart_id AND product_id = $prod");
+    $gq_fetch = $get_quant->fetch_assoc();
+    $quant = $gq_fetch['quantity'];
+    $add_item = array($prod => $quant);
+    $new_item_count = array_replace($items_in_cart, $add_item);
+    $_SESSION['items_in_cart'] = $new_item_count;
+
+
+    header("Location:extrasidor.php?id=$product_id");
+
+}
 
     include("onatop.php");
     $sql = "SELECT * FROM product WHERE product_id = $product_id";
@@ -73,7 +107,13 @@ if( isset($_GET['add']) )
     $fetch = $res->fetch_assoc();
 
     $product_name = $fetch['product_name'];
-    $stock = $fetch['stock'];
+    $real_stock = $fetch['stock'];
+    $items_in_cart = $_SESSION['items_in_cart'][$product_id];
+    if($real_stock >= $items_in_cart) {
+        $stock = $real_stock - $_SESSION['items_in_cart'][$product_id];
+    }else{
+        $stock = 0;
+    }
     $price = $fetch['price'];
     $product_info = $fetch['product_info'];
     $img = "img/" . $product_name . ".png";
